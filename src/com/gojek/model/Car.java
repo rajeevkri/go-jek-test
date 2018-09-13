655812bc094b58148b87ec5870eee3f28f795b9b
Rajeev krishna Singh <srajeev@athenahealth.com>
2018-09-13 14:15:36 +0530

    adding car model

---
+++ src/com/gojek/model/Car.java
@@ -0,0 +1,28 @@
+package com.gojek.model;
+
+public class Car {
+	String regNo;
+	String color;
+	
+	public Car(String regNo, String color) {
+        this.regNo = regNo;
+        this.color = color;
+    }
+
+	public String getRegNo() {
+		return regNo;
+	}
+
+	public void setRegNo(String regNo) {
+		this.regNo = regNo;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+}
